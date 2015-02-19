55b80a169bd86ae2b4acd61a08a56b1895fa6123
Adam Hutchison <liveh2o@gmail.com>
2015-02-18 21:36:20 -0700

    Use the new release task pattern
    Bundler 1.8 changed the release task to call other tasks and removed the
    method that we were dependent on here. Update the `install` method to
    include the tasks normally installed by Bundler. Also add a new
    geminabox_push task to handle pushing to geminabox instead of overriding
    Bundler's rubygems_push.

--- lib/special_delivery/gem_helper.rb
+++ lib/special_delivery/gem_helper.rb
@@ -3,25 +3,43 @@ require 'bundler/gem_helper'
 module SpecialDelivery
   class GemHelper < ::Bundler::GemHelper
     def install
+      built_gem_path = nil
+
       desc "Build #{name}-#{version}.gem into the pkg directory."
       task 'build' do
-        build_gem
+        built_gem_path = build_gem
       end
 
       desc "Build and install #{name}-#{version}.gem into system gems."
-      task 'install' do
-        install_gem
+      task 'install' => 'build' do
+        install_gem(built_gem_path)
       end
 
-      desc "Create tag #{version_tag} and build and push #{name}-#{version}.gem to your geminabox server"
-      task 'release' do
-        release_gem
+      desc "Create tag #{version_tag} and build and push #{name}-#{version}.gem to Rubygems"
+      task 'release' => ['build', 'release:guard_clean',
+                         'release:source_control_push', 'release:geminabox_push'] do
       end
 
-      ::Bundler::GemHelper.instance = self
+      task 'release:guard_clean' do
+        guard_clean
+      end
+
+      task 'release:source_control_push' do
+        tag_version { git_push } unless already_tagged?
+      end
+
+      task 'release:geminabox_push' do
+        geminabox_push(built_gem_path) if gem_push?
+      end
+
+      task 'release:rubygem_push' do
+        rubygem_push(built_gem_path) if gem_push?
+      end
+
+      GemHelper.instance = self
     end
 
-    def rubygem_push(path)
+    def geminabox_push(path)
       if Pathname.new("~/.gem/geminabox").expand_path.exist?
         sh("gem inabox #{path}")
         ::Bundler.ui.confirm "Pushed #{name} #{version} to your geminabox server."
